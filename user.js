3eaae62de7286bfdbf785ce18fd75b39e2cd99bf
Joe Yates <joe.g.yates@gmail.com>
2015-07-13 14:28:37 +0200

    Put the settings in a user.js file

---
+++ user.js
@@ -0,0 +1,12 @@
+user_pref("browser.pocket.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.enabled", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("datareporting.healthreport.service.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("geo.enabled", false);
+user_pref("loop.enabled", false);
+user_pref("media.eme.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
